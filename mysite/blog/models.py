564c3d289ff023469057582c26a6b38a4567e153
atrust@sajonara.de <atrust@sajonara.de>
2025-07-13 12:06:36 +0200

    made changes in views to reflext new seo friendly url

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -58,4 +58,4 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse("blog:post_detail", args=[self.published_at.year, self.published_at.month, self.published_at.day, self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -12,10 +12,13 @@ def post_list(request):
         {'posts': posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
+        slug=post,
+        published_at__year=year,
+        published_at__month=month,
+        published_at__day=day,
         status=Post.Status.DRAFT
     )
     return render(
