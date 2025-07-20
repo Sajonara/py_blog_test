d31c54a269670728e63bab02ff220a74922226cf
atrust@sajonara.de <atrust@sajonara.de>
2025-07-20 16:28:16 +0200

    add comment model and make migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -59,3 +59,22 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.published_at.year, self.published_at.month, self.published_at.day, self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_at']
+        indexes = [
+            models.Index(fields=['created_at']),
+        ]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
+    
